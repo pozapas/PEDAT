b80f64d168cc2ba631d1ab580cca453c92e29ec3
Amir Rafe <amir.rafe@usu.edu>
2023-08-08 19:47:34 -0600

    Update dash_beta.py

--- dash_beta.py
+++ dash_beta.py
@@ -1178,10 +1178,11 @@ def main():
 
             # Add selected signals
             pdf.set_font('Arial', 'B', 14)
-            pdf.cell(0, 10, 'Selected Signals:', ln=True, align='L')
+            pdf.ln(10) 
+            pdf.cell(100, 10, 'Selected Signals:', ln=True, align='L')
             pdf.set_font('Arial', '', 12)
             for signal in selected_signals:
-                pdf.cell(0, 6, '- ' + signal, ln=True, align='L')
+                pdf.cell(100, 6, '- ' + signal, ln=True, align='L')
 
             # Add duration
             pdf.set_font('Arial', 'B', 14)
